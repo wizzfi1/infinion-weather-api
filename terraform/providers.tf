6b19d01ae6a36f0192854dac87ea3f23975e8d2c
shaibuuneks <shaibuuneks@gmail.com>
2025-10-18 21:41:30 +0000

    terraform providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,24 +1,17 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
+
+  required_version = ">= 1.5.0"
 }
 
 provider "azurerm" {
   features {}
 
-  # These will automatically read from environment variables if not set
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  # Use Azure CLI or Cloud Shell authentication
+  use_cli = true
 }
