1974be8681fe341a3015fbcb791ef3b268fa9fdf
shaibuuneks <shaibuuneks@gmail.com>
2025-10-18 10:21:05 +0000

    sub ID configuration

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,15 +1,3 @@
 terraform {
-  required_version = ">= 1.1.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
-  backend "local" {}
+  required_version = ">= 1.5.0"
 }
