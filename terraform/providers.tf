afa1932b3f50181ab22e080148126baec79de4a9
shaibuuneks <shaibuuneks@gmail.com>
2025-10-18 10:19:12 +0000

    sub ID configuration

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
 provider "azurerm" {
   features {}
+  use_cli         = true
+  subscription_id = var.subscription_id
+  tenant_id       = "215b7ce2-5263-4593-a622-da030405d151"
 }
